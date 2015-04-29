8d30d593461f9bdc75a3dc01958f6db53e5362b3
Tristan Marsh <tristanmarsh@live.com>
2015-04-29 21:31:42 +1000

    Wordpress Updates + Cake Database

--- dash/.tools/monish_cake_db.sql
+++ dash/.tools/monish_cake_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2015 at 07:05 AM
+-- Generation Time: Apr 29, 2015 at 05:58 AM
 -- Server version: 5.6.17
 -- PHP Version: 5.5.12
 
@@ -17,8 +17,31 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `buildzeroseven`
+-- Database: `buildzeroeight`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `emergencies`
+--
+
+CREATE TABLE IF NOT EXISTS `emergencies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `phone` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `emergencies`
+--
+
+INSERT INTO `emergencies` (`id`, `first_name`, `last_name`, `phone`, `email`) VALUES
+(1, 'Jake', 'Mate', 812903, 'abc@gmail.com'),
+(2, 'Manny', 'Man', 8129032, 'abccc@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -33,7 +56,14 @@ CREATE TABLE IF NOT EXISTS `emergency_contacts` (
   `phone` int(11) NOT NULL,
   `email` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `emergency_contacts`
+--
+
+INSERT INTO `emergency_contacts` (`id`, `first_name`, `last_name`, `phone`, `email`) VALUES
+(1, 'John', 'Smith', 412345678, 'john@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -75,14 +105,7 @@ CREATE TABLE IF NOT EXISTS `internet_connection` (
   `status` enum('ACTIVE','INACTIVE') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `lease_id` (`lease_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `internet_connection`
---
-
-INSERT INTO `internet_connection` (`id`, `lease_id`, `bandwidth`, `monthly_fee`, `date_start`, `date_end`, `status`) VALUES
-(1, 1, '1GB', '', '2015-04-23', '2015-09-23', '');
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -101,14 +124,18 @@ CREATE TABLE IF NOT EXISTS `leases` (
   PRIMARY KEY (`id`),
   KEY `room_id` (`room_id`),
   KEY `student_id` (`student_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
 --
 -- Dumping data for table `leases`
 --
 
 INSERT INTO `leases` (`id`, `room_id`, `student_id`, `date_start`, `date_end`, `lease_status`, `weekly_price`) VALUES
-(1, 1, 1, '2015-04-23', '2015-10-23', 'ONGOING', 200);
+(4, 1, 4, '2015-04-29', '2015-04-29', 'EXPIRED', 12),
+(5, 1, 5, '2015-04-29', '2015-04-29', 'ONGOING', 123),
+(6, 1, 4, '2015-04-29', '2015-04-29', 'ONGOING', 2147483647),
+(7, 13, 4, '2015-04-29', '2015-04-29', 'EXPIRED', 67),
+(8, 1, 4, '2015-04-29', '2015-04-29', 'EXPIRED', 243);
 
 -- --------------------------------------------------------
 
@@ -124,7 +151,7 @@ CREATE TABLE IF NOT EXISTS `maintenances` (
   `modified` datetime DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=16 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=17 ;
 
 --
 -- Dumping data for table `maintenances`
@@ -137,7 +164,8 @@ INSERT INTO `maintenances` (`id`, `title`, `description`, `created`, `modified`,
 (12, 'Broken Window', 'Got broken into and robbed ', '2015-04-13 15:18:31', '2015-04-13 15:18:31', 6),
 (13, 'Broken Airconditioner', 'Aircon does not turn on', '2015-04-14 03:50:50', '2015-04-14 03:50:50', 10),
 (14, 'Broken Face', 'got bashed', '2015-04-16 05:25:43', '2015-04-16 05:25:43', 24),
-(15, 'Broken dong', 'donger is broken ', '2015-04-22 10:57:33', '2015-04-22 10:57:33', 29);
+(15, 'Broken dong', 'donger is broken ', '2015-04-22 10:57:33', '2015-04-22 10:57:33', 29),
+(16, 'Broken Borken', 'Borken', '2015-04-25 09:33:29', '2015-04-25 09:33:29', 30);
 
 -- --------------------------------------------------------
 
@@ -177,7 +205,7 @@ CREATE TABLE IF NOT EXISTS `properties` (
 --
 
 INSERT INTO `properties` (`id`, `address`, `number_rooms`, `bathrooms`, `kitchens`, `storeys`, `garage`) VALUES
-(1, '70 something street', 5, 1, 1, 1, '');
+(1, '70 something street', 5, 1, 1, 1, 'TRUE');
 
 -- --------------------------------------------------------
 
@@ -188,17 +216,19 @@ INSERT INTO `properties` (`id`, `address`, `number_rooms`, `bathrooms`, `kitchen
 CREATE TABLE IF NOT EXISTS `rooms` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `property_id` int(11) NOT NULL,
+  `room_name` varchar(50) NOT NULL,
   `vacant` enum('TRUE','FALSE') NOT NULL,
   PRIMARY KEY (`id`),
   KEY `property_id` (`property_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=14 ;
 
 --
 -- Dumping data for table `rooms`
 --
 
-INSERT INTO `rooms` (`id`, `property_id`, `vacant`) VALUES
-(1, 1, '');
+INSERT INTO `rooms` (`id`, `property_id`, `room_name`, `vacant`) VALUES
+(1, 1, 'unit 1 room 1', 'TRUE'),
+(13, 1, 'unit 1 room 2', 'TRUE');
 
 -- --------------------------------------------------------
 
@@ -210,18 +240,19 @@ CREATE TABLE IF NOT EXISTS `students` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `expected_grad_date` date NOT NULL,
   `person_id` int(11) NOT NULL,
-  `country_of_birth` varchar(20) NOT NULL,
+  `emergency_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `person_id` (`person_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+  KEY `person_id` (`person_id`),
+  KEY `emergency_id` (`emergency_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `students`
 --
 
-INSERT INTO `students` (`id`, `expected_grad_date`, `person_id`, `country_of_birth`) VALUES
-(1, '2015-10-23', 28, 'china'),
-(2, '2015-04-23', 29, 'hong kong');
+INSERT INTO `students` (`id`, `expected_grad_date`, `person_id`, `emergency_id`) VALUES
+(4, '2015-04-27', 29, 1),
+(5, '2015-04-27', 30, 1);
 
 -- --------------------------------------------------------
 
@@ -248,7 +279,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `postcode` int(15) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=30 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=33 ;
 
 --
 -- Dumping data for table `users`
@@ -256,8 +287,11 @@ CREATE TABLE IF NOT EXISTS `users` (
 
 INSERT INTO `users` (`id`, `username`, `password`, `role`, `created`, `modified`, `title`, `first_name`, `last_name`, `gender`, `phone`, `email`, `home_country`, `city`, `suburb`, `postcode`) VALUES
 (1, 'admin', '$2y$10$ytpEvi6EvojZvBDYa18lDezDToFfQ9e64lD.gIo0r30z9uscH4xXy', 'admin', NULL, NULL, 'MR', 'Tony', 'Wise', 'M', 0, '', '', '', '', 0),
-(28, 'tenant50', '$2y$10$rsTuv7CgtHwhyjtV4BqK9Om2W33vRJKlVs1SZpP/fxeImQt1YXJHK', 'author', '2015-04-22 10:38:52', '2015-04-22 10:38:52', 'MR', 'jacob', '', 'M', 0, '', '', '', '', 0),
-(29, 'tenant51', '$2y$10$T2vn/qK4X/1r7h6QifMMyu229y5zDSSUAakejLXeB.Fhufop.rL5q', 'author', '2015-04-22 10:57:05', '2015-04-22 10:57:05', 'MR', 'mike', 'lai', 'M', 0, '', '', '', '', 0);
+(28, 'tenant50', '$2y$10$rsTuv7CgtHwhyjtV4BqK9Om2W33vRJKlVs1SZpP/fxeImQt1YXJHK', 'tenant', '2015-04-22 10:38:52', '2015-04-27 03:32:15', 'MR', 'Jacob', 'Ben', 'M', 412345678, 'BenJacobs@gmail.com', 'America', 'New York', 'First Avenue', 190283),
+(29, 'tenant51', '$2y$10$T2vn/qK4X/1r7h6QifMMyu229y5zDSSUAakejLXeB.Fhufop.rL5q', 'tenant', '2015-04-22 10:57:05', '2015-04-27 03:43:26', 'MR', 'Michael', 'Lai', 'M', 418273645, 'michael@gmail.com', 'France', 'Paris', 'hawhawhaw', 9123),
+(30, 'tenant1', '$2y$10$9oL26vXvDX7JJsxH6JSTwOR/Ht75vkgoxzt1QFE.isPzZgURDQOTq', 'tenant', '2015-04-25 09:33:08', '2015-04-25 09:33:08', 'MISS', 'Jenny', 'Law', 'F', 48921783, 'jenny@gmail.com', 'China', 'Guang Zhou', 'Dong Shan', 1239081),
+(31, 'tenant4', '$2y$10$G435ZwjIKY3iWF4Bz4mESO1ktRAOyGhSfQPaR9MHTcCtnvlfU1liu', 'tenant', '2015-04-28 08:55:16', '2015-04-28 08:55:16', 'MISS', 'Fiona', 'Lopez', 'F', 433646343, 'fiona@gmail.com', 'Atlanta', 'Submarine', 'Tank', 123123),
+(32, 'tenant6', '$2y$10$UXBGmOHBT8kH3iOGT8CckuHD8DXR/ev8F9T4Vb.KmAyw2g00PTb0K', 'tenant', '2015-04-29 01:58:45', '2015-04-29 01:58:45', 'MR', 'Ben', 'Ten', 'M', 482818293, 'ben@gmail.com', 'Switz', 'cityname', 'suburbname', 31023);
 
 --
 -- Constraints for dumped tables
@@ -305,7 +339,8 @@ ALTER TABLE `rooms`
 -- Constraints for table `students`
 --
 ALTER TABLE `students`
-  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `users` (`id`);
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`emergency_id`) REFERENCES `emergencies` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

efaa5d160eb36ea0ffb8df8fff73cf28da156ea2
Tristan Marsh <tristanmarsh@live.com>
2015-09-02 22:42:25 +1000

    Javascript Scroll Header Shadow

--- dash/webroot/js/custom.js
+++ dash/webroot/js/custom.js
@@ -14,3 +14,10 @@ Since:    31/7/15
 // jQuery(window).load(function($) {
 // 	NProgress.done();
 // });
+
+//This is supposed to apply and remove the style when the scrollbar is not at the top and back at the top, respectively
+//Note to self. Remove all CDN stuff. fonts/etc/
+$(document).ready(function() {
+	window.console.log('go');
+	$('.header').css('box-shadow', '0 5px 10px 0px rgba(118, 170, 219, 0.8)');
+})(jQuery);

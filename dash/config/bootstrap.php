94b01fa6b8ed84a24a022fa4f2bcf3dffca62f61
Tristan Marsh <tristanmarsh@live.com>
2015-10-15 12:56:44 +1100

    Debug Kit Disabled

--- dash/config/bootstrap.php
+++ dash/config/bootstrap.php
@@ -185,7 +185,7 @@ Plugin::load('Utils');
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
 if (Configure::read('debug')) {
-    Plugin::load('DebugKit', ['bootstrap' => true]);
+    Plugin::load('DebugKit', ['bootstrap' => false]);
 }
 
 /**

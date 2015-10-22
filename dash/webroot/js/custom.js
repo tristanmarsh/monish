7cf16d254c960364e0af6619088738833ec77956
Tristan Marsh <tristanmarsh@live.com>
2015-10-23 00:34:21 +1100

    Branding my name on the style

--- dash/webroot/js/custom.js
+++ dash/webroot/js/custom.js
@@ -1,9 +1,9 @@
 /*
 Name:   	custom.js
-Author:   Team Dash
+Author:   	Team Dash
 Author URI: http://wiki.tristanmarsh.net/dash/
-Purpose:  All custom scripting for Monish Dashboard
-Since:    31/7/15
+Purpose:  	All custom scripting for Monish Dashboard
+Since:    	31/7/15
 */
 
 // jQuery(document).ready(function($) {
